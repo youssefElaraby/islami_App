b74ec03e07062d49590f8aa4432682f24601125f
Youssef Elaraby <155681877+youssefelaraby@users.noreply.github.com>
2024-07-26 18:17:30 +0300

    add qurqn tap and hadeth tap and sepha tap

--- lib/sepha/sepha_tap.dart
+++ lib/sepha/sepha_tap.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:islami/colors_App.dart';
 
-class SephaTap extends StatelessWidget {
+class SephaTap extends StatefulWidget {
+  @override
+  State<SephaTap> createState() => _SephaTapState();
+}
+
+class _SephaTapState extends State<SephaTap> {
+  int counter = 0;
+
+  List<String> azkar = [
+    "سبحان الله",
+    "الحمد لله",
+    "لااله الا الله",
+    "الله اكبر"
+  ];
+  int index = 0;
+  double angle = 0;
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
@@ -8,28 +25,89 @@ class SephaTap extends StatelessWidget {
     return Container(
       child: Column(
         children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Positioned(
-                top: screenHeight * 0.055,
-                child: Image.asset(
-                  'assets/images/head of seb7a.png',
-                  width: screenWidth * 0.17, // Use a percentage of screen width
+          Center(
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Image.asset('assets/images/head of seb7a.png'),
+                // SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.all(35.0),
+                  child: Transform.rotate(
+                    angle: angle,
+                    child: InkWell(
+                      onTap: () {
+                        clickOnImage();
+                        angle += 3.14 / 4;
+
+                        setState(() {});
+                      },
+                      child: Image.asset('assets/images/body of seb7a-1.png'),
+                    ),
+                  ),
                 ),
-              ),
-              Positioned(
-                top: screenWidth * 0.19,
-                child: Image.asset(
-                  'assets/images/body of seb7a-1.png',
-                  width: screenWidth * 0.81, // Use a percentage of screen width
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
           Text('عدد التسبيحات '),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: AppColors.primaryLightColor,
+                ),
+                // height: 95,
+                width: screenWidth * 0.3,
+                child: Center(child: Text('$counter')),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: AppColors.primaryLightColor,
+                ),
+                // height: 95,
+                width: screenWidth * 0.6,
+                child: Center(child: Text('${azkar[index]}')),
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  incrementCount() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  clickOnImage() {
+    counter++;
+    //دي علشان لما الكونتر يجيب33يزود ال indexواحد
+    if (counter % 33 == 0) {
+      index++;
+    }
+    //دي علشان لما يخلص الليست ال هما اربع ازكار
+    //يرجه تاني يجيب من اول زكر ويخلي الكونتر ب 0
+    if (azkar.length == index) {
+      index = 0;
+      counter = 0;
+    }
+    //دي علشان لما الكونتر يجيب 33 يرجع 0تاني
+    if (counter == 33) {
+      counter = 0;
+    }
   }
 }
