f469c8ae3deefa2bc36658637520ce60edeb773a
Lenovo <youssefel3raby777@gmail.com>
2024-07-26 15:08:21 +0300

    Init Project. Added empty home screen.

--- lib/sepha/sepha_tap.dart
+++ lib/sepha/sepha_tap.dart
@@ -6,23 +6,28 @@ class SephaTap extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     return Container(
-      child: Stack(
-        alignment: Alignment.center,
+      child: Column(
         children: [
-          Positioned(
-            top: screenHeight * 0.055,
-            child: Image.asset(
-              'assets/images/head of seb7a.png',
-              width: screenWidth * 0.17, // Use a percentage of screen width
-            ),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: screenHeight * 0.055,
+                child: Image.asset(
+                  'assets/images/head of seb7a.png',
+                  width: screenWidth * 0.17, // Use a percentage of screen width
+                ),
+              ),
+              Positioned(
+                top: screenWidth * 0.19,
+                child: Image.asset(
+                  'assets/images/body of seb7a-1.png',
+                  width: screenWidth * 0.81, // Use a percentage of screen width
+                ),
+              ),
+            ],
           ),
-          Positioned(
-            top: screenWidth * 0.19,
-            child: Image.asset(
-              'assets/images/body of seb7a-1.png',
-              width: screenWidth * 0.81, // Use a percentage of screen width
-            ),
-          ),
+          Text('عدد التسبيحات '),
         ],
       ),
     );
